6e03f2fa14dbdc5015dfcaf593555cac6106a3d7
MoyTW <moytravis@gmail.com>
2016-11-18 22:16:10 -0800

    Add Verb for killing non-pawn targets in melee
    It one-shots all non-pawn targets. Whew! That's pretty scary!
    
    Incidentally you can actually use it to mine with, if you can stand the,
    uh, literally designating every square to attack.

---
+++ Source/MTW_Treasures/Verb_MeleeAttackDestroyStructures.cs
@@ -0,0 +1,25 @@
+﻿using Verse;
+using RimWorld;
+
+namespace MTW_Treasures
+{
+    class Verb_MeleeAttackDestroyStructures : Verb_MeleeAttack
+    {
+        protected override bool TryCastShot()
+        {
+            if (base.TryCastShot())
+            {
+                Thing target = this.currentTarget.Thing;
+                if (!(target is Pawn))
+                {
+                    target.Destroy(DestroyMode.Kill);
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
