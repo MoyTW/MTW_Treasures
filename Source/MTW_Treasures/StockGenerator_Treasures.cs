78c14ab80128f06dcc940a11381fbc151ddd8307
MoyTW <moytravis@gmail.com>
2016-11-20 15:36:04 -0800

    Change stop-gen criteria to "Thing Already Exists"
    Removes the "is stockable" check.

--- Source/MTW_Treasures/StockGenerator_Treasures.cs
+++ Source/MTW_Treasures/StockGenerator_Treasures.cs
@@ -9,24 +9,31 @@ namespace MTW_Treasures
 {
     public class StockGenerator_Treasures : StockGenerator
     {
+        private Thing TryMakeForStockSingle(ThingDef thingDef)
+        {
+            ThingDef stuff = null;
+            if (thingDef.MadeFromStuff)
+            {
+                stuff = (from st in DefDatabase<ThingDef>.AllDefs
+                         where st.IsStuff && st.stuffProps.CanMake(thingDef)
+                         select st).RandomElementByWeight((ThingDef st) => st.stuffProps.commonality);
+            }
+            Thing thing = ThingMaker.MakeThing(thingDef, stuff);
+            thing.stackCount = 1;
+            return thing;
+        }
+
+        private bool CanStockTreasure(ThingDef def)
+        {
+            return Find.ListerThings.ThingsOfDef(def).Count == 0;
+        }
+
         public override IEnumerable<Thing> GenerateThings()
         {
             // Always generates 1 of every treasure at the moment.
-            foreach (var def in TreasuresUtils.AllTreasuresDefs)
+            foreach (var def in TreasuresUtils.AllTreasuresDefs.Where(d => this.CanStockTreasure(d)))
             {
-                if (def.tradeability != Tradeability.Stockable)
-                {
-                    break;
-                }
-                else
-                {
-                    IEnumerator<Thing> enumerator = StockGeneratorUtility.TryMakeForStock(def, 1).GetEnumerator();
-                    while (enumerator.MoveNext())
-                    {
-                        Thing current = enumerator.Current;
-                        yield return current;
-                    }
-                }
+                yield return this.TryMakeForStockSingle(def);
             }
             yield break;
         }
